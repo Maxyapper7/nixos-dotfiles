b06d4de3b1cfab3e04390d669364b30e11d224e8
Maxyapper7 <thisisreal@example.com>
2025-03-16 10:08:27 -0600

    Added Install script

--- flake.nix
+++ flake.nix
@@ -21,31 +21,57 @@
 
   outputs = { self, nixpkgs, home-manager, hyprland, nixos-hardware, auto-cpufreq, stylix, nvf, ... }@inputs:
     let
-    system = "x86_64-linux";
-  lib = nixpkgs.lib;
-  pkgs = nixpkgs.legacyPackages.${system};
-  in {
-    nixosConfigurations = {
-      nixos = lib.nixosSystem {
-        inherit system;
-        specialArgs = { inherit inputs; };
-        modules = [
-          ./configuration.nix
-          nixos-hardware.nixosModules.framework-12th-gen-intel
-          stylix.nixosModules.stylix
-        ];
+      system = "x86_64-linux";
+      lib = nixpkgs.lib;
+      pkgs = nixpkgs.legacyPackages.${system};
+      supportedSystems = [ "aarch64-linux" "i686-linux" "x86_64-linux" ];
+      forAllSystems = inputs.nixpkgs.lib.genAttrs supportedSystems;
+      nixpkgsFor =
+        forAllSystems (system: import inputs.nixpkgs { inherit system; });
+    in {
+      nixosConfigurations = {
+        nixos = lib.nixosSystem {
+          inherit system;
+          specialArgs = { inherit inputs; };
+          modules = [
+            ./configuration.nix
+            nixos-hardware.nixosModules.framework-12th-gen-intel
+            stylix.nixosModules.stylix
+          ];
+        };
       };
-    };
 
-    homeConfigurations = {
-      max = home-manager.lib.homeManagerConfiguration {
-        inherit pkgs;
-        extraSpecialArgs = { inherit inputs; };
-        modules = [
-          ./home.nix 
-          inputs.nvf.homeManagerModules.default
-        ];
+      homeConfigurations = {
+        max = home-manager.lib.homeManagerConfiguration {
+          inherit pkgs;
+          extraSpecialArgs = { inherit inputs; };
+          modules = [
+            ./home.nix 
+            inputs.nvf.homeManagerModules.default
+          ];
+        };
       };
+
+      packages = forAllSystems (system:
+        let pkgs = nixpkgsFor.${system};
+        in {
+          default = self.packages.${system}.install;
+
+          install = pkgs.writeShellApplication {
+            name = "install";
+            runtimeInputs = with pkgs; [ git vim ];
+            text = ''${./install.sh} "$@"'';
+          };
+        });
+
+      apps = forAllSystems (system: {
+        default = self.apps.${system}.install;
+
+        install = {
+          type = "app";
+          program = "${self.packages.${system}.install}/bin/install";
+        };
+      });
+
     };
-  };
 }
