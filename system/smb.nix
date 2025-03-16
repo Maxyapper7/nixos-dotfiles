6cef74e7932e3d641ee31c3c5ffcb2188924029c
Maxyapper7 <thisisreal@example.com>
2025-03-16 13:35:36 -0600

    Moved SMB share mount

--- system/smb.nix
+++ system/smb.nix
@@ -3,7 +3,7 @@
 {
 
   environment.systemPackages = [ pkgs.cifs-utils ];
-  fileSystems."/mnt/Toronto" = {
+  fileSystems."/home/max/Toronto" = {
     device = "//toronto/Max";
     fsType = "cifs";
     options = [ "username=max" "password=T#Ld*9PtS8^Mr3X4tZaU" ];
