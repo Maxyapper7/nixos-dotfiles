ca5573114dc25cf981f82b44495a03d6abb520a5
Maxyapper7 <thisisreal@example.com>
2025-03-16 13:48:28 -0600

    Updated install.sh

--- install.sh
+++ install.sh
@@ -24,17 +24,19 @@ else
     sed -i "0,/grubDevice.*=.*\".*\";/s//grubDevice = \"\/dev\/$grubDevice\";/" $SCRIPT_DIR/flake.nix
 fi
 
+sed -i "0,/emmet/s//$(whoami)/" $SCRIPT_DIR/flake.nix
+sed -i "0,/Emmet/s//$(getent passwd $(whoami) | cut -d ':' -f 5 | cut -d ',' -f 1)/" $SCRIPT_DIR/flake.nix
+sed -i "s/emmet@librephoenix.com//" $SCRIPT_DIR/flake.nix
+sed -i "s+~/.dotfiles+$SCRIPT_DIR+g" $SCRIPT_DIR/flake.nix
+
 # Open up editor to manually edit flake.nix before install
 if [ -z "$EDITOR" ]; then
-    EDITOR=vim;
+    EDITOR=nano;
 fi
 $EDITOR $SCRIPT_DIR/flake.nix;
 
 # Rebuild system
-sudo nixos-rebuild switch --flake /home/max/dotfiles;
+sudo nixos-rebuild switch --flake $SCRIPT_DIR#system;
 
 # Install and build home-manager configuration
-nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
-nix-channel --update
-nix-shell '<home-manager>' -A install
-home-manager switch --flake ~/dotfiles
+nix run home-manager/master --extra-experimental-features nix-command --extra-experimental-features flakes -- switch --flake $SCRIPT_DIR#user;
