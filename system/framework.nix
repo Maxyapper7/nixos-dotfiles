94ea34eaf066b6a31ade961155ddc373d2e99eeb
Maxyapper7 <thisisreal@example.com>
2025-03-23 19:11:28 -0600

    Updated Framework Config

--- system/framework.nix
+++ system/framework.nix
@@ -2,14 +2,55 @@
 
 {
 
-  imports =
-    [
-      ./battery.nix
-    ];
+  ### Battery ###
+  hardware.opengl.extraPackages = with pkgs; [
+    mesa_drivers
+    vaapiIntel
+    vaapiVdpau
+    libvdpau-va-gl
+    intel-media-driver
+  ];
 
-  services.fwupd.enable = true;
+  powerManagement = {
+    enable = true;
+    powertop.enable = true;
+    cpuFreqGovernor = lib.mkDefault "ondemand";
+  };
+
+  services.thermald.enable = true;
+
+  boot.kernelPackages = pkgs.linuxPackagesFor (pkgs.linux_latest.override {
+    argsOverride = rec {
+      src = pkgs.fetchurl {
+        url = "mirror://kernel/linux/kernel/v5.x/linux-${version}.tar.xz";
+        sha256 = "1j0lnrsj5y2bsmmym8pjc5wk4wb11y336zr9gad1nmxcr0rwvz9j";
+      };
+      version = "5.15.1";
+      modDirVersion = "5.15.1";
+    };
+  });
+
+  programs.auto-cpufreq = {
+    enable = true;
+    settings = {
+      charger = {
+        governor = "performance";
+        turbo = "auto";
+      };
+
+      battery = {
+        governor = "powersave";
+        turbo = "auto";
+      };
+    };
+
+    boot.kernelParams = [ "mem_sleep_default=deep" ];
+
+  };
+
 
   ### Fingerprint BS ###
+  services.fwupd.enable = true;
 
   services.fprintd = {
     enable = true;
